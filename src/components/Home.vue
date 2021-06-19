de62536514f4d7cb7099406883a8087983c6ad19
gonior <arthemuss14@gmail.com>
2021-06-19 09:19:12 +0700

    init git

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-app id="home">
+        <v-main style="height:100vh;" class="purple d-flex align-center justify-center " >
+            <div class="green pa-2 ">
+                <span>Hello World</span>
+            </div>
+            
+        </v-main>
+    </v-app>
+</template>
+<script>
+
+export default {
+    name : "Home",
+    
+}
+</script>
